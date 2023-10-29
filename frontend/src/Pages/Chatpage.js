d6d4d038889e7b0870e09a580b0f7bf214239dc0
Sheelu kumar <sheeluofficial@gmail.com>
2023-10-30 00:57:27 +0530

    added proxy url to backend

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,6 +1,13 @@
+import axios from 'axios'
 import React from 'react'
 
-const Chatpage = () => {
+const Chatpage =  () => {
+   axios.get("/cc").then((data)=>{
+    console.log(data)
+ }).catch((error)=>{
+    console.log(error)
+ })
+
   return (
     <div>Chatpage</div>
   )
