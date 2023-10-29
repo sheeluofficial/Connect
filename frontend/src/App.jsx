1335fd08ed849e6d7a7f6b001e2183ce5663ad79
Sheelu kumar <sheeluofficial@gmail.com>
2023-10-29 21:55:13 +0530

    re-factor initial backend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import { Box } from '@chakra-ui/react'
+import axios from "axios"
 function App() {
 
 
+  const fetchData  = async ()=>{
+    const data = await axios.get("/");
+    console.log(data);
+  }
+
+  useEffect(()=>{
+ fetchData();
+
+  },[]);
+
   return (
     <>
+    <Box color={"red"}>
+
      <h1>hi</h1>
+    </Box>
     </>
   )
 }
